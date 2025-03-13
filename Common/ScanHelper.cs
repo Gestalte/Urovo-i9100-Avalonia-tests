662f0244333228f101e72b29c464d04217e01a3f
Gestalte <66849891+Gestalte@users.noreply.github.com>
2025-03-13 10:52:55 +0200

    Add Common project

---
+++ Common/ScanHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Common
+{
+    public static class ScanHelper
+    {
+        public static Action? StartScanning { get; set; }
+        public static Action? StopScanning { get; set; }
+
+        public static Action<string?>? ScanResult { get; set; }
+    }
+}
