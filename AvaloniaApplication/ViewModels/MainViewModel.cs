bb2d65e1d0147cc22f948829657bed64bdd9b6a1
Gestalte <66849891+Gestalte@users.noreply.github.com>
2025-03-13 11:30:23 +0200

    Add UI code to call scanning code

--- AvaloniaApplication/ViewModels/MainViewModel.cs
+++ AvaloniaApplication/ViewModels/MainViewModel.cs
@@ -1,6 +1,51 @@
-﻿namespace AvaloniaApplication.ViewModels;
+﻿using Common;
+using ReactiveUI;
+
+namespace AvaloniaApplication.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private string? scanResult;
+    public string? ScanResult
+    {
+        get => scanResult;
+        set => this.RaiseAndSetIfChanged(ref scanResult, value);
+    }
+
+    private string scanButtonText= "Start scanning";
+    public string ScanButtonText
+    {
+        get => scanButtonText;
+        set => this.RaiseAndSetIfChanged(ref scanButtonText, value);
+    }
+
+    bool nowScanning = false;
+    public void StartScan()
+    {
+        void scanResult(string? result)
+        {
+            if (result is null)
+            {
+                return;
+            }
+            ScanResult = result;
+            ScanButtonText = "Start scanning";
+            ScanHelper.StopScanning?.Invoke();
+        }
+
+        this.nowScanning = !this.nowScanning;
+
+        if (this.nowScanning)
+        {
+            ScanButtonText = "Stop scanning";
+
+            ScanHelper.ScanResult = scanResult;
+            ScanHelper.StartScanning?.Invoke();
+        }
+        else
+        {
+            ScanButtonText = "Start scanning";
+            ScanHelper.StopScanning?.Invoke();
+        }
+    }
 }
